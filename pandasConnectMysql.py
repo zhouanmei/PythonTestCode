9cdbc12e96a7e6a25afed0f687855e732d9a3574
zhouanmei <33830541+zhouanmei@users.noreply.github.com>
2017-11-20 19:31:05 +0800

    testEdit

--- pandasConnectMysql.py
+++ pandasConnectMysql.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 pymysql.install_as_MySQLdb()
 import MySQLdb
 mysql_cn=MySQLdb.connect(host="localhost",port=3306,user="root",passwd="",db="test",charset="utf8")
-df=pd.read_sql("select * from jandan_duan limit 15",con=mysql_cn)
+df=pd.read_sql("select * from jandan_duan",con=mysql_cn)
 #print(df)
 #plt.plot(df["duanTucao"])
 #plt.show()
